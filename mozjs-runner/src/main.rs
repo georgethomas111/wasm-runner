4b9dee23776480a491919acf81f0606625b69489
george <george@pop-os.localdomain>
2023-02-18 00:13:27 -0800

    Simple addition script works. Next step is figuring out something more interesting

--- mozjs-runner/src/main.rs
+++ mozjs-runner/src/main.rs
@@ -1,16 +1,39 @@
-use std::io;
+use std::ptr;
+
+use mozjs::rooted;
+
+use mozjs::jsapi::JS_NewGlobalObject;
+use mozjs::jsapi::OnNewGlobalHookOption;
+
+use mozjs::jsval::UndefinedValue;
+
+
+use mozjs::rust::JSEngine;
+use mozjs::rust::RealmOptions;
+use mozjs::rust::Runtime;
+use mozjs::rust::SIMPLE_GLOBAL_CLASS;
+
+fn run(rt: Runtime) {
+  let context = rt.cx();
+  let options = RealmOptions::default();
+
+  rooted!(in(context) let global = unsafe {
+    JS_NewGlobalObject(context, &SIMPLE_GLOBAL_CLASS, ptr::null_mut(),
+                           OnNewGlobalHookOption::FireOnNewGlobalHook,
+                           &*options)
+  });
+
+  rooted!(in(context) let mut rval = UndefinedValue());
+  rt.evaluate_script(global.handle(), "123+123", "test", 1, rval.handle_mut());
+  let res = rval.get().to_int32();
+  println!("Result of script = {res}")
+}
 
 fn main() {
-  println!("Guess the number");
-  println!("Enter the number");
+
+  let engine = JSEngine::init().expect("failed to initalize JS engine");
+  let runtime = Runtime::new(engine.handle());
   
   let mut guess = String::new();
-
-  io::stdin().read_line(&mut guess).expect("Error reading input");
-
-  println!("You guessed {guess}");
-
-  let guess_int: u32 = guess.trim().parse().unwrap();
-  let abc = guess_int * 10;
-  println!("abc = {abc}");
+  run(runtime);
 }
