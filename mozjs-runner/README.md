4f1848eb1a1ce0f2a2a93432b26488c1e69d5c77
George Thomas <gerogethomas.mec@gmail.com>
2023-02-26 00:45:50 -0800

    Get the string out of Spidermonkey and return it

--- mozjs-runner/README.md
+++ mozjs-runner/README.md
@@ -2,6 +2,7 @@
 
 This is a project attempting to run spidermonkey similar to how node js runs v8. 
 
+
 # What do we have?
 
 There is a program which creates a run time and then runs a small script inline and returns a value.
@@ -13,8 +14,36 @@ $ cargo build
 
 ```
 
+# Building
+```
+$ cargo build
+```
+
 # Running
 
 ```
-$ ./target/debug/mozjs-runner
+$ ./target/debug/mozjs-runner "`cat jsExp/utcDate.js`"
+Input script -> "let d = new Date()\nd.toUTCString()"
+Sun, 26 Feb 2023 08:41:15 GMT
 ```
+
+```
+$ ./target/debug/mozjs-runner "1000 * 5"
+Input script -> "1000 * 5"
+5000
+```
+
+# TODO
+
+* Get a script to output Date.now()
+  >> Done
+* Figure out how to get character out the string length.
+  >> Done
+* Figure out how to get character out of the spider monkey heap.
+  >> Done
+* Write a function to convert JSString to rust string.
+
+* Accept javascript input as command line argument.
+
+* Get a script new Date(Date.now()) to work
+
--- mozjs-runner/src/main.rs
+++ mozjs-runner/src/main.rs
@@ -1,10 +1,12 @@
 use std::ptr;
+use std::env;
 
 use mozjs::rooted;
 
 use mozjs::jsapi::JS_NewGlobalObject;
 use mozjs::jsapi::OnNewGlobalHookOption;
-
+use mozjs::jsapi::JS_GetStringLength;
+use mozjs::jsapi::JS_GetStringCharAt;
 use mozjs::jsval::UndefinedValue;
 
 
@@ -13,7 +15,7 @@ use mozjs::rust::RealmOptions;
 use mozjs::rust::Runtime;
 use mozjs::rust::SIMPLE_GLOBAL_CLASS;
 
-fn run(rt: Runtime) {
+fn run(rt: Runtime, js: &str) {
   let context = rt.cx();
   let options = RealmOptions::default();
 
@@ -24,16 +26,46 @@ fn run(rt: Runtime) {
   });
 
   rooted!(in(context) let mut rval = UndefinedValue());
-  rt.evaluate_script(global.handle(), "123+123", "test", 1, rval.handle_mut());
-  let res = rval.get().to_int32();
-  println!("Result of script = {res}")
+  rt.evaluate_script(global.handle(), js, "test", 1, rval.handle_mut());
+  if rval.get().is_number() {
+    let date_now = rval.get().to_number();
+    println!("{}", date_now)
+  }
+
+  if rval.get().is_string() {
+    let ptr_str = rval.get().to_string();
+    unsafe {
+      let length = JS_GetStringLength(ptr_str);
+      let mut x:u16 = 0;
+      let char_val = &mut x as *mut u16;
+      let mut i = 0;
+      let mut strs:Vec<u16> = vec![];
+      loop {
+        JS_GetStringCharAt(context, ptr_str, i, char_val);
+        strs.push(*char_val);
+        
+        i+=1;
+        if i == (length) {
+          break;
+        }
+      }
+
+      let s = String::from_utf16(&strs).unwrap();
+      println!("{}", s)
+    }
+  }
 }
 
 fn main() {
-
+  let args: Vec<String> = env::args().collect();
+  if args.len() < 2 {
+    println!("Please pass javascript to execute in quotes as the second argument");
+    return
+  }
+  let js: &str = &args[1][..];
+  println!("Input script -> {:?}", js);
   let engine = JSEngine::init().expect("failed to initalize JS engine");
   let runtime = Runtime::new(engine.handle());
   
-  let mut guess = String::new();
-  run(runtime);
+  run(runtime, js);
 }
